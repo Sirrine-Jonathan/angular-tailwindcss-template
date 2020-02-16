91d3f5cfd3582d0c1ce204b05c5e3bc81563202a
Jonathan <sir15004@byui.edu>
2020-02-15 17:43:40 -0800

    removed heroe stuff and made it more into a generic template

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,13 +3,11 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
-import { HeroesComponent } from './heroes/heroes.component';
 import { TitleBannerComponent } from './title-banner/title-banner.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeroesComponent,
     TitleBannerComponent
   ],
   imports: [
